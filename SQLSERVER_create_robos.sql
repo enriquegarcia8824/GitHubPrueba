d0cf7eaaaee36b2dedd433e1a3d0ac9333294d45
enriquegarcia8824 <80009429+enriquegarcia8824@users.noreply.github.com>
2021-03-04 18:32:54 -0500

    agregar archivos

---
+++ SQLSERVER_create_robos.sql
@@ -0,0 +1,17 @@
+create table robos(
+Documento		int not null,
+Fecha			date, 
+Departamento	VARCHAR (50),
+Municipio		VARCHAR (50),
+Dia				VARCHAR (9),
+Hora			time,
+Sitio			VARCHAR (50),
+Agresor			VARCHAR (50),
+Victima			VARCHAR (50),
+Edad			int,
+Pais_nacimiento	VARCHAR (50),
+Clase_empleado	VARCHAR (50),
+Profesion		VARCHAR (50),
+Escolaridad		VARCHAR (50),
+Codigo_DANE		int
+);
